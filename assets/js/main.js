8fa89226d28cd8a925ea9982892b8d07000d4ce4
HelljumperJ <jefrey.poveda17@gmail.com>
2024-01-22 16:07:08 -0500

    darkmode

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,29 +1,44 @@
 
-    // ----------------------- BUSQUEDA POR INPUT -------------------------------------------
-
 document.addEventListener('DOMContentLoaded', function () {
     var searchInput = document.querySelector('input[type="text"]');
     var flagCards = document.querySelectorAll('.card');
+    var regionSelect = document.querySelector('select');
 
-    searchInput.addEventListener('input', function () {
-        var searchTerm = searchInput.value.toLowerCase();
+    // Evento de cambio para el select
+    regionSelect.addEventListener('change', function () {
+        var selectedRegion = regionSelect.value.toLowerCase();
 
         flagCards.forEach(function (card) {
-            var flagTitle = card.querySelector('.card-title').innerText.toLowerCase();
-            if (flagTitle.includes(searchTerm)) {
+            var flagRegion = card.querySelector('.text-gray-500').innerText.toLowerCase();
+            if (selectedRegion === 'all' || flagRegion === selectedRegion) {
                 card.classList.remove('hidden');
             } else {
                 card.classList.add('hidden');
             }
         });
     });
-});
 
-// -------------------------------- MODO OSCURO ---------------------------------------
+    // Evento de entrada para el input de búsqueda
+    searchInput.addEventListener('input', function () {
+        var searchTerm = searchInput.value.toLowerCase();
 
-document.addEventListener('DOMContentLoaded', function () {
+        flagCards.forEach(function (card) {
+            var flagTitle = card.querySelector('.card-title').innerText.toLowerCase();
+            var flagRegion = card.querySelector('.text-gray-500').innerText.toLowerCase();
+            if (flagTitle.includes(searchTerm) && (regionSelect.value === 'all' || flagRegion === regionSelect.value.toLowerCase())) {
+                card.classList.remove('hidden');
+            } else {
+                card.classList.add('hidden');
+            }
+        });
+    });
+
+    // Evento de cambio para el modo oscuro
     var darkModeCheckbox = document.querySelector('input[type="checkbox"]');
     var body = document.body;
+    var navbar = document.querySelector('.navbar');
+    var select = document.querySelector('.select');
+    var input = document.querySelector('.input');
 
     darkModeCheckbox.addEventListener('change', function () {
         body.classList.toggle('dark-mode', darkModeCheckbox.checked);
